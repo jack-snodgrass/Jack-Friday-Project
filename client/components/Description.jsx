31afe62ef2d8aa603fe298a44a08c61ebf8af9d6
jack-snodgrass <69236334+jack-snodgrass@users.noreply.github.com>
2020-11-06 11:23:40 +1300

    Wrote media query for mobile sized screens

--- client/components/Description.jsx
+++ client/components/Description.jsx
@@ -4,25 +4,25 @@ import genres from '../../data/genres'
 
 // Create Post template to be used in AllPosts parent instance of posts
 const Description = props => {
-  const genre = genres[props.match.params.genre].find(genre => genre.name===props.match.params.name)  
+  const genre = genres[props.match.params.genre].find(genre => genre.name === props.match.params.name)
   const songs = genre.songs
-  // console.log(JSON.stringify(obj))       
-          // return <Description key={genre.id} name={genre.name} description={genre.description}/>
-        
-        // )
+  // console.log(JSON.stringify(obj))
+  // return <Description key={genre.id} name={genre.name} description={genre.description}/>
+
+  // )
   console.log('description props:', props)
   return (
-    <div className='description'>
+    <div className='col-3 description'>
       <h2>{props.match.params.name}</h2>
-      <p> {genre.description}</p> 
+      <p> {genre.description}</p>
       <h3>Songs</h3>
       <ul>
-      {songs.map(song => {
+        {songs.map(song => {
           return <li key={song}>{song}</li>
         }
         )}
-        
-      </ul>  
+
+      </ul>
     </div>
   )
 }
