24704fa579c20f508f5d7223690d7a7a159c0d26
Shelly (Michelle) Mutu-Grigg <55133797+shellymutu-grigg@users.noreply.github.com>
2020-11-06 08:25:39 +1300

    update solution to add in two tests to add unit testing

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,9 +2,16 @@ import React from 'react'
 import { shallow } from 'enzyme'
 
 import App from '../client/components/App'
+import Nav from '../client/components/Nav'
 
-test('<App />', () => {
-  const expected = 'React development has begun!'
+test('<App /> returns main header text', () => {
+  const expected = 'Welcome to BEATLE TUNES!!<Nav /><Route /><Route /><Route />'
   const wrapper = shallow(<App />)
+  expect(wrapper.text()).toBe(expected)
+})
+
+test('<Nav /> returns nav header', () => {
+  const expected = 'Genre<Genre /><Genre /><Genre /><Genre />'
+  const wrapper = shallow(<Nav />)
   expect(wrapper.text()).toBe(expected)
 })
